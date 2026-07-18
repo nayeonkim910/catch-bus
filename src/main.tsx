94bc20b6f4066fdbc30cf2a99170f88a2c558bd1
김나연 <yoni91059105@gmail.com>
2026-07-18 12:55:38 +0900

    feat: Pretendard 폰트 적용 및 디자인 시스템 토큰 구축 (#14)
    * feat: Pretendard 폰트 적용
    * feat: 디자인 시스템 토큰 구축 및 다크 테마 지원

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import 'pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryProvider } from './app/QueryProvider.tsx';
